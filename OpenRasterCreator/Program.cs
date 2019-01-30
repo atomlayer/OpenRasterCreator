e39eadb2e122c0fa9072bf50b4a105f456dc0052
anfulu36484 <anfulu36484@gmail.com>
2019-01-30 16:59:43 +0300

    Update test data

--- OpenRasterCreator/Program.cs
+++ OpenRasterCreator/Program.cs
@@ -11,24 +11,7 @@ namespace OpenRasterCreator
     {
         static void Main(string[] args)
         {
-            /* //Test data
-             List<Bitmap> list = new List<Bitmap>();
-             list.Add(new Bitmap("img0.png"));
-             list.Add(new Bitmap("img1.png"));
-             list.Add(new Bitmap("img2.png"));
-
-             var openRaster = new Openraster(
-                                  new LayerFolder("testFolder",
-                                      new Layer(list[1],"test1"),
-                                      new Layer(list[2], "test2"),
-                                      new LayerFolder("testFolder2",
-                                          new Layer(list[0], "test1"))),
-                                  new Layer(list[0], "test1")   
-                                  );
-
-             openRaster.Save(@"output.ora");*/
-
-            //Test data
+            //****Example
             List<Bitmap> list = new List<Bitmap>();
             list.Add(new Bitmap("img0.png"));
             list.Add(new Bitmap("img1.png"));
@@ -45,47 +28,6 @@ namespace OpenRasterCreator
 
             openRaster.Save(@"output.ora");
 
-            /*for (int i = 0; i < 2; i++)
-            {
-                var groupFolder = new LayerFolder("testGroup");
-
-                
-                    var lineFolder = new LayerFolder("line");
-                    lineFolder.AddNodeToEndPos(new Layer(list[2], "line"));
-                    groupFolder.AddNodeToEndPos(lineFolder);
-
-              
-                    groupFolder.AddNodeToEndPos(new Layer(list[2], "shadow"));
-                   
-             
-                    groupFolder.AddNodeToEndPos(new Layer(list[2], "base"));
-              
-                
-                rendring.AddNodeToEndPos(groupFolder);
-
-            }
-
-            openraster.AddNodeToEndPos(rendring);
-            openraster.BuildFile();
-
-
-
-            /*List<Bitmap> list =new List<Bitmap>();
-            list.Add(new Bitmap("layer0.png"));
-            list.Add(new Bitmap("layer1.png"));
-            list.Add(new Bitmap("layer2.png"));
-
-            Openraster openraster =new Openraster();
-
-            openraster.AddNodeToEndPos(new OpenRasterNode(list[0],"first"));
-
-            var folder = new LayerFolder("FolderTest");
-            folder.AddNodeToEndPos(new OpenRasterNode(list[1], "second"));
-            folder.AddNodeToEndPos(new OpenRasterNode(list[2], "third"));
-
-            openraster.AddNodeToEndPos(folder);
-
-            openraster.BuildFile();*/
         }
     }
 }
